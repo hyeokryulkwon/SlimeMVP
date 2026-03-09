21854c79746d0ca83b64cee037bd62d8dcbd04ab
Hyeokryul <jjang94111@gmail.com>
2026-03-10 00:19:11 +0900

    Initial Commit

---
+++ SlimeMVP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlimeMVP
+//
+//  Created by 권혁률 on 3/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlimeMVP/SlimeMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimeMVPApp.swift
+//  SlimeMVP
+//
+//  Created by 권혁률 on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SlimeMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
